2df67c758ac824314e09c2c3f60fb18f0274dd69
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2025-03-03 20:53:09 +0100

    kronos: allow the core only for x86_64

--- packages/lakka/libretro_cores/kronos/package.mk
+++ packages/lakka/libretro_cores/kronos/package.mk
@@ -3,43 +3,32 @@ PKG_VERSION="58352d6dc969fa90c5fa1220f38ffe577157547f"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/FCare/Kronos"
 PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_ARCH="x86_64"
+PKG_DEPENDS_TARGET="toolchain kronos:host"
+PKG_DEPENDS_HOST="toolchain:host"
 PKG_LONGDESC="Port of Kronos to libretro."
 PKG_TOOLCHAIN="make"
 
-PKG_MAKE_OPTS_TARGET="-C ../yabause/src/libretro HAVE_CDROM=1"
+PKG_MAKE_OPTS_TARGET="-C yabause/src/libretro platform=unix HAVE_CDROM=1"
 
 if [ "${OPENGL_SUPPORT}" = "yes" ]; then
   PKG_DEPENDS_TARGET+=" ${OPENGL}"
-  PKG_MAKE_OPTS_TARGET+=" HAVE_OPENGL=1"
-fi
-
-if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
-  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
-  PKG_MAKE_OPTS_TARGET+=" FORCE_GLES=1"
 fi
 
 if [ "${VULKAN_SUPPORT}" = "yes" ]; then
   PKG_DEPENDS_TARGET+=" ${VULKAN}"
 fi
 
-if [ "${ARCH}" = "arm" ]; then
-  if [ "${PROJECT}" = "Samsung" -a "${DEVICE}" = "Exynos" ]; then
-    PKG_MAKE_OPTS_TARGET+=" platform=odroid BOARD=ODROID-XU4"
-  else
-    PKG_MAKE_OPTS_TARGET+=" platform=armv"
-  fi
-elif [ "${ARCH}" = "aarch64" ]; then
-  PKG_MAKE_OPTS_TARGET+=" platform=arm64"
-fi
-
-pre_make_target() {
-  mkdir ${PKG_BUILD}/build_retro
-  cd ${PKG_BUILD}/build_retro
+make_host() {
   make ${PKG_MAKE_OPTS_TARGET} generate-files
+}
+
+makeinstall_host() {
+ # nothing to do
+ :
 }
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v ../yabause/src/libretro/kronos_libretro.so ${INSTALL}/usr/lib/libretro/
+    cp -v yabause/src/libretro/kronos_libretro.so ${INSTALL}/usr/lib/libretro/
 }
