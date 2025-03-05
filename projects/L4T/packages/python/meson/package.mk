5ad288dbc53498e55bd6d2ce2fd169979fbda4b5
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2025-03-05 09:41:06 +0100

    meson: add package from v6.x to L4T due to compatibility
    + updated to 1.7.0

---
+++ projects/L4T/packages/python/meson/package.mk
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="meson"
+PKG_VERSION="1.7.0"
+PKG_SHA256="08efbe84803eed07f863b05092d653a9d348f7038761d900412fddf56deb0284"
+PKG_LICENSE="Apache"
+PKG_SITE="https://mesonbuild.com"
+PKG_URL="https://github.com/mesonbuild/meson/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="Python3:host setuptools:host"
+PKG_LONGDESC="High productivity build system"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_host() {
+  exec_thread_safe python3 setup.py install --prefix=${TOOLCHAIN} --skip-build
+}
+
+post_makeinstall_host() {
+  # Avoid using full path to python3 that may exceed 128 byte limit.
+  # Instead use PATH as we know our toolchain is first.
+  sed -e '1 s/^#!.*$/#!\/usr\/bin\/env python3/' -i ${TOOLCHAIN}/bin/meson
+}
